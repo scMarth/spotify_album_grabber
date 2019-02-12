8057446afa297f67a79394ba1e8560d355e354d7
scMarth <vlantaca@gmail.com>
2019-02-11 22:52:27 -0800

    testing

--- spotify_album_thumb_grabber.py
+++ spotify_album_thumb_grabber.py
@@ -4,8 +4,11 @@ with open(r'./album_urls.txt', 'r') as file:
     for line in file:
         r = requests.get(line)
         expression = r'<meta[\s]+property="og:image"[\s]+content="(http[^"]+)"'
-        img_url = re.findall(expression, r.text)[0]
-        if img_url:
+        matches = re.findall(expression, r.text)
+        if matches:
+            img_url = matches[0]
             expression = r'([^/]+)$'
             img_name = re.findall(expression, img_url)[0]
             urllib.request.urlretrieve(img_url, img_name + ".jpg")
+        else:
+            
