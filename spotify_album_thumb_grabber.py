c5ec0d30461f7589dbb6cb383c8024e26a2df287
scMarth <vlantaca@gmail.com>
2019-02-13 15:29:51 -0800

    update todo, code cleanup/comment

--- spotify_album_thumb_grabber.py
+++ spotify_album_thumb_grabber.py
@@ -2,18 +2,17 @@ import requests, re, urllib
 
 with open(r'./album_urls.txt', 'r') as file:
     for line in file:
-        r = requests.get(line.strip())
-        expression = r'<meta[\s]+property="og:image"[\s]+content="(http[^"]+)"'
-        matches = re.findall(expression, r.text)
-        if matches:
-            img_url = matches[0]
-            expression = r'([^/]+)$'
-            img_name = re.findall(expression, img_url)[0]
-            urllib.request.urlretrieve(img_url, img_name + ".jpg")
-        else:
+        r = requests.get(line.strip()) # Get the HTML
+        try:
+            expression = r'<meta[\s]+property="og:image"[\s]+content="(http[^"]+)"'
+            img_url = re.findall(expression, r.text)[0]             # Find image url from the HTML
+            img_name = re.findall(r'([^/]+)$', img_url)[0] + ".jpg" # Make a filename based on the url
+            urllib.request.urlretrieve(img_url, img_name)           # Download the image
+        except:
             try:
-                debug_filename = re.findall(r'([^/]+)$', line.strip())[0]
-                with open(debug_filename + ".txt", 'w') as debug_file:
+                # If something went wrong, try to dump the result of the request
+                debug_filename = re.findall(r'([^/]+)$', line.strip())[0] + ".txt"
+                with open(debug_filename, 'w') as debug_file:
                     debug_file.write(r.text)
             except:
                 continue
