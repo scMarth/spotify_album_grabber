1439d29006d245327a8577bfb38014bac68f9a09
scMarth <vlantaca@gmail.com>
2019-11-29 23:24:26 -0800

    add config options to change output directory

--- spotify_album_thumb_grabber.py
+++ spotify_album_thumb_grabber.py
@@ -1,13 +1,21 @@
-import requests, re, urllib
+import requests, re, urllib, os
+
+# use this to specify the output path of downloaded thumb images
+config = {
+    'specify_path' : False,
+    'album_art_path' : r''
+}
+
 
 with open(r'./album_urls.txt', 'r') as file:
     for line in file:
         r = requests.get(line.strip()) # Get the HTML
         try:
             expression = r'<meta[\s]+property="og:image"[\s]+content="(http[^"]+)"'
-            img_url = re.findall(expression, r.text)[0]             # Find image url from the HTML
-            img_name = re.findall(r'([^/]+)$', img_url)[0] + ".jpg" # Make a filename based on the url
-            urllib.request.urlretrieve(img_url, img_name)           # Download the image
+            img_url = re.findall(expression, r.text)[0]                     # Find image url from the HTML
+            img_name = re.findall(r'([^/]+)$', img_url)[0] + ".jpg"         # Make a filename based on the url
+            file_path = config['album_art_path'] + '\\' + img_name if config['specify_path'] else os.path.dirname(__file__) + '\\' + img_name
+            urllib.request.urlretrieve(url=img_url, filename=file_path)      # Download the image
         except:
             try:
                 # If something went wrong, try to dump the result of the request
